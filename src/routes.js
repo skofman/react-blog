d2199c55c51de2f26d1c92924da9ac40bbba51ae
Shlomo Kofman <shlomo.kofman@gmail.com>
2016-08-20 13:37:10 -0700

    Done

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app.js';
 import PostsIndex from './components/posts.index.js';
+import PostsNew from './components/posts.new.js';
+import PostsShow from './components/posts.show.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
